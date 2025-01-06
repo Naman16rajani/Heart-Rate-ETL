3b86c99a9c5566f3d0b54cf3d536a1a15b36a16e
naman16rajani <naman16rajani@gmail.com>
2025-01-05 22:54:49 -0800

    Transformation done v1.0

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="orchestrate",
+    packages=find_packages(exclude=["orchestrate_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud",
+        "dagster-airbyte",  
+        "dagster-dbt",      
+        "dbt-core==1.7.3",         
+        "dbt-snowflake==1.7.0", 
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
